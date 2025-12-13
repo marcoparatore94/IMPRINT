dad39c56ced650e8a0669bff85a313aac8fdbdbe
marcoparatore94 <marcoparatore.94@gmail.com>
2025-12-13 21:45:54 +0100

    Update model_loader.py

--- IMPRINT-app/model_loader.py
+++ IMPRINT-app/model_loader.py
@@ -2,10 +2,9 @@ import os
 import joblib
 
 def load_model():
-    model_path = r"C:\Users\marco\PycharmProjects\IMPRINT-app\models\imprint_extended.pkl"
+    base_dir = os.path.dirname(__file__)
+    model_path = os.path.join(base_dir, "models", "imprint_extended.pkl")
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"Model not found at: {model_path}")
     bundle = joblib.load(model_path)
-    if not all(k in bundle for k in ["model","predictors","transforms"]):
-        raise KeyError("Bundle must contain 'model', 'predictors', and 'transforms'.")
     return bundle["model"], bundle["predictors"], bundle["transforms"]
